92731f5702667a7af5a131fa76143d4940d8c23d
fox3nox <142689196+fox3nox@users.noreply.github.com>
2026-08-25 21:54:37 +0200

    Refresh RECLAIM offline cache

--- reclaim/sw.js
+++ reclaim/sw.js
@@ -1,4 +1,4 @@
-const CACHE='reclaim-v1';
+const CACHE='reclaim-v2';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
